cd7f81065b3f4296fcbc97be4157ce9c8daf313e
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-06 20:14:23 +0000

    Added first version of the crowdfunding smart contract

--- smart_contracts/contracts/Crowdfunding.sol
+++ smart_contracts/contracts/Crowdfunding.sol
@@ -3,14 +3,28 @@ pragma solidity ^0.8.13;
 
 contract Crowdfunding {
     struct Box {
-        /// The percentage a box adds up to the total
-        uint8 percentage;
         /// Title of a box
         string title;
         /// Description of a box
         string description;
+        /// The percentage a box adds up to the total
+        uint8 percentage;
         /// The price of this box in eth
         uint256 price;
+    }
+
+    struct BoxSell {
+        /// Buyer of the box
+        address buyer;
+        /// Sold box
+        Box box;
+    }
+
+    struct BoxOffer {
+        /// The box to sell
+        Box box;
+        /// Number of available boxes of this kind
+        uint256 available;
     }
 
     struct Campaign {
@@ -20,18 +34,16 @@ contract Crowdfunding {
         string title;
         /// The campaigns description
         string description;
-        /// Target amount in eth of this campaign
-        uint256 targetAmount;
         /// Total amount collected in eth
         uint256 collectedAmount;
+        /// Sold percentage
+        uint8 progress;
         /// The deadline of this campaign
         uint256 deadline;
-        /// List of all buyers
-        address[] buyers;
-        /// List of all available boxes
-        Box[] availableBoxes;
-        /// List of all sold boxes
-        Box[] soldBoxes;
+        /// Available boxes
+        BoxOffer[] availableBoxes;
+        /// All sold boxes
+        BoxSell[] soldBoxes;
     }
 
     // This allows to access campaigns like campaigns[0]
@@ -39,13 +51,15 @@ contract Crowdfunding {
 
     uint256 public numberOfCampaigns = 0;
 
+    /**
+     * Creates a new campaign
+     */
     function createCampaign(
         address _owner,
         string memory _title,
         string memory _description,
-        uint256 _targetAmount,
         uint256 _deadline,
-        Box[] memory _boxes
+        BoxOffer[] memory _boxes
     ) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
@@ -58,13 +72,17 @@ contract Crowdfunding {
         // all boxes have to add up to 100
         uint8 totalPercentage = 0;
         for (uint256 index = 0; index < _boxes.length; index++) {
-            Box memory box = _boxes[index];
+            BoxOffer memory offer = _boxes[index];
+            require(offer.available >= 1, "There must be at least one box to sell");
+
+            Box memory box = offer.box;
             // A box must add some percentage to the total
             require(
                 box.percentage > 0 && box.percentage <= 100,
                 "The percentage of a box can't must be higher than 0 and lower or equal to 100"
             );
-            totalPercentage += _boxes[index].percentage;
+
+            totalPercentage += box.percentage;
         }
         require(
             totalPercentage == 100,
@@ -75,10 +93,11 @@ contract Crowdfunding {
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
-        campaign.targetAmount = _targetAmount;
         campaign.collectedAmount = 0;
+        campaign.progress = 0;
         campaign.deadline = _deadline;
         campaign.availableBoxes = _boxes;
+        campaign.soldBoxes = new BoxSell[](_boxes.length);
 
         // increase total num of campaigns
         numberOfCampaigns++;
@@ -87,31 +106,53 @@ contract Crowdfunding {
         return numberOfCampaigns - 1;
     }
 
-    function buyBox() {}
+    /**
+     * Buys a box
+     */
+    function buyBox(uint256 _campaignId, uint256 _boxId) public payable {
+        uint256 amount = msg.value;
 
-    function getCampaigns() {}
+        // get desired campaign and box
+        Campaign storage campaign = campaigns[_campaignId];
+        require(campaign.deadline <= block.timestamp, "This campaign is over");
+        BoxOffer storage boxOffer = campaign.availableBoxes[_boxId];
+        require(boxOffer.available > 0, "All boxes of this type have been sold");
+        Box memory box = boxOffer.box;
+        require(box.price <= amount, "Given amount is below box price");
 
-    /*address owner;
-    uint256 deadline;
-    string title;
-    string description;
-    Box[] availableBoxes;
-    Box[] soldBoxes;*/
+        // Reduce available boxes by one
+        boxOffer.available--;
+        // Add to total percentage
+        campaign.progress+=box.percentage;
+        // Add total collected amount
+        campaign.collectedAmount+=amount;
+        // Add buyer
+        BoxSell memory boxSell = BoxSell({buyer: msg.sender, box: box});
+        campaign.soldBoxes.push(boxSell);
 
-    /*constructor(string memory _title, string memory _description, uint256 _days, Box[] memory _boxes) {
-        owner = msg.sender;
-        title = _title;
-        description = _description;
-        
-        uint8 totalPercentage = 0;
-        for (uint256 index = 0; index < _boxes.length; index++) {
-            Box memory box = _boxes[index];
-            // A box must add some percentage to the total
-            require(box.percentage > 0 && box.percentage <= 100);
-            totalPercentage+=_boxes[index].percentage;
+        // TODO Check if percentage is 100%, if yes -> start chain
+    }
+
+    /**
+     * Returns a specific campaign
+     */
+    function getCampaign(uint256 _id) public view returns (Campaign memory) {
+        Campaign storage campaign = campaigns[_id];
+        return campaign;
+    }
+
+    /**
+     * Returns all campaigns
+     */
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint256 index = 0; index < numberOfCampaigns; index++) {
+            Campaign storage campaign = campaigns[index];
+            allCampaigns[index] = campaign;
         }
-        // total percentage must be 100
-        require(totalPercentage == 100);
-        availableBoxes = _boxes;
-    }*/
+
+        return allCampaigns;
+    }
+    
 }
