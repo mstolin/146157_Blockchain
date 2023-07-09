fd7ff43eb2fe1af1cfdfcbc293625051d50f4be6
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-09 15:03:11 +0000

    Added interface for winow.ethereum

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+declare global {
+  interface Window { ethereum?: any; }
+}
+window.ethereum = window.ethereum || {};
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
