6e1f879dd4d194200633538f8f0ed55b4039a88e
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-09 19:20:06 +0000

    Subscribed to wallet to set default account if needed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
+import { WalletActions } from './state/wallet.actions';
 import { selectWallet } from './state/wallet.selectors';
 
 @Component({
@@ -10,12 +11,24 @@ import { selectWallet } from './state/wallet.selectors';
 })
 export class AppComponent {
   title = 'frontend';
+
   links: { title: string, link: string }[] = [
     { 'title': 'Home', 'link': '' },
     { 'title': 'All Campaigns', 'link': 'all-campaigns' },
     { 'title': 'New Campaign', 'link': 'new-campaign' },
   ];
+
   wallet$ = this.store.select(selectWallet);
+
+  ngOnInit(): void {
+    this.wallet$.subscribe(wallet => {
+      if (wallet.activeAccount == null && wallet.accounts.length > 0) {
+        // set default active account
+        const firstAccount = wallet.accounts[0];
+        this.store.dispatch(WalletActions.setActiveAccount({ account: firstAccount }));
+      }
+    });
+  }
 
   constructor(private store: Store) {}
 }
