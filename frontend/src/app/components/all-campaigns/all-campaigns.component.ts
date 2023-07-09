55a90c32961baf20e2fbe72cf9edbd5b48c222be
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-09 08:49:29 +0000

    Showed some dummy components

--- frontend/src/app/components/all-campaigns/all-campaigns.component.ts
+++ frontend/src/app/components/all-campaigns/all-campaigns.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AllCampaignsComponent {
 
+  campaigns: { title: string, description: string }[] = [
+    {
+      'title': 'Campaign #1',
+      'description': 'A description for #1'
+    },
+    {
+      'title': 'Campaign #2',
+      'description': 'A description for #2'
+    },
+  ];
+
+  constructor() {}
+
 }
