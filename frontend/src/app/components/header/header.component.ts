c784bdf3c2101e3e4bb64a54c6138201e8678a29
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-09 15:03:41 +0000

    Added possibility to login via metamask

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import detectEthereumProvider from '@metamask/detect-provider'
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,35 @@ export class HeaderComponent {
 
   @Input() links!: { title: string, link: string }[];
 
+  hasProvider = false;
+
+  // TODO move to store
+  wallet: { accounts: [] } = { 'accounts': [] };
+
+  ngOnInit(): void {
+    this.detectProvider();
+  }
+
   constructor(public route: ActivatedRoute) {}
+
+  private detectProvider() {
+    detectEthereumProvider({ silent: true }).then(provider => {
+      this.hasProvider = Boolean(provider);
+    }).catch(() => {
+      this.hasProvider = false;
+    });
+  }
+
+  async connectWallet() {
+    let accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    this.wallet.accounts = accounts;
+
+    /*
+    TODO now set store accounts to accounts
+    this way we now if login was successful
+    */
+  }
 
 }
