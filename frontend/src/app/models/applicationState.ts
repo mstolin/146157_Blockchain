4aba32fba93856ed230842efd2aebd203b2c680e
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-09 16:09:05 +0000

    Added first models

---
+++ frontend/src/app/models/applicationState.ts
@@ -0,0 +1,5 @@
+import Wallet from "./wallet";
+
+export default interface ApplicationState {
+  wallet: Wallet;
+}
---
+++ frontend/src/app/models/wallet.ts
@@ -0,0 +1,13 @@
+export default class Wallet {
+
+  private _accounts: string[] = [];
+
+  set accounts(accounts: string[]) {
+    this._accounts = accounts;
+  }
+
+  get accounts() {
+    return this._accounts;
+  }
+
+}
