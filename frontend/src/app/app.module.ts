e0e0aa441b09b44c781184a5e08b89203dacf8ef
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-10 08:24:56 +0000

    Imported forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +29,7 @@ import { AccountSelectorComponent } from './components/account-selector/account-
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    FormsModule,
     StoreModule.forRoot({ wallet: walletReducer }),
   ],
   providers: [],
