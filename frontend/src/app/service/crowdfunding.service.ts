5bb9358a4b9e2e53083cf6002cbc3eb67d054bd2
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-11 20:13:34 +0000

    Fixed how to receive campaigns

--- frontend/src/app/service/crowdfunding.service.ts
+++ frontend/src/app/service/crowdfunding.service.ts
@@ -1,13 +1,21 @@
 // @ts-nocheck
 
 import { Injectable } from '@angular/core';
-import { Web3, Web3EthInterface, eth } from 'web3';
+import { Web3 } from 'web3';
 
 import Campaign from '../models/campaign';
 import Crowdfunding from '../../assets/abi/Crowdfunding.json';
 import BoxOffer from '../models/boxOffer';
 import Box from '../models/box';
-import { Observable } from 'rxjs';
+
+type CampaignRes = {
+  title: string;
+  description: string;
+  owner: string;
+  progress: bigint;
+  deadline: bigint;
+  collectedAmount: bigint;
+};
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +27,15 @@ export class CrowdfundingService {
   private contract?: Web3EthInterface.Contract;
 
   constructor() {
-
+    const { ethereum } = window as any;
+    if (ethereum) {
+      this.web3 = new Web3(ethereum);
+      this.selectedAddress = Web3.utils.toChecksumAddress(ethereum.selectedAddress);
+      this.contract = new this.web3.eth.Contract(
+        Crowdfunding.abi,
+        '0xb9d1f4d6878d22aa50c1da31f40b8c3a88b31b5e',
+      );
+    }
   }
 
   async createCampaign(campaign: Campaign): Promise<boolean> {
@@ -49,21 +65,24 @@ export class CrowdfundingService {
       .send({ 'from': selectedAddress });
   }
 
-  async getCampaigns(): Observable<Campaign> {
-    const { ethereum } = window as any;
-    const web3 = new Web3(ethereum);
+  async getCampaigns(): Promise<any[]> {
+    return new Promise(async (resolve, reject) => {
+      if (this.contract) {
+        try {
+          let campaigns: CampaignRes[] = await this.contract
+            .methods
+            .getCampaigns()
+            .call();
+          campaigns = campaigns.map(campaign => new Date(Number(campaign.deadline) * 1000));
 
-    const contract = new web3.eth.Contract(
-      Crowdfunding.abi,
-      '0xb9d1f4d6878d22aa50c1da31f40b8c3a88b31b5e',
-    );
-
-    contract
-      .methods
-      .getCampaigns()
-      .call()
-      .then(res => console.log("RES", res))
-      .catch(err => console.log("ERR", err));
+          resolve(campaigns);
+        } catch(err) {
+          reject(err);
+        }
+      } else {
+        reject();
+      }
+    });
   }
 
 }
