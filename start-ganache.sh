d3c00edd75eb76208ab46cd971d4b970881995b5
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-11 09:10:14 +0000

    Added script to start ganache from cli

---
+++ start-ganache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+port="${1:-8545}"
+mnemonic="deny vault maximum sort call drama barrel tongue upon equal tennis equip"
+
+ganache -p "${port}" -m "${mnemonic}" --db /workspaces/146157_Blockchain/ganache/
