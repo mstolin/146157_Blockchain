b238180424817b168f22cf5099f682e858ea609a
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-12 07:41:00 +0000

    Extended campaign model

--- frontend/src/app/models/campaign.ts
+++ frontend/src/app/models/campaign.ts
@@ -1,13 +1,19 @@
 export default class Campaign {
 
-  private _title: string;
-  private _description: string;
-  private _owner: string;
+  private readonly _title: string;
+  private readonly _description: string;
+  private readonly _owner: string;
+  private readonly _progress: number;
+  private readonly _deadline: Date;
+  private readonly _collectedAmount: bigint;
 
-  constructor(title: string, description: string, owner: string) {
+  constructor(title: string, description: string, owner: string, progress: number, deadline: Date, collectedAmount: bigint) {
     this._title = title;
     this._description = description;
     this._owner = owner;
+    this._progress = progress;
+    this._deadline = deadline;
+    this._collectedAmount = collectedAmount;
   }
 
   get title() {
@@ -20,6 +26,18 @@ export default class Campaign {
 
   get owner() {
     return this._owner;
+  }
+
+  get progress() {
+    return this._progress;
+  }
+
+  get deadline() {
+    return this._deadline;
+  }
+
+  get collectedAmount() {
+    return this._collectedAmount;
   }
 
 }
