bee14cac16f89199d72eb764ecf4dee41f70d648
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-12 15:56:23 +0000

    Fixed ganache scripts

--- ganache/start-ganache.sh
+++ ganache/start-ganache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 port="${1:-8545}"
+mnemonic="deny vault maximum sort call drama barrel tongue upon equal tennis equip"
 
-ganache -p "${port}"
+ganache -p "${port}" -m "${mnemonic}" --db $(pwd)/debug/
