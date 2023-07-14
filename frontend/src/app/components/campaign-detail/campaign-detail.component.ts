b80e5c9418f956a85fd1c2b553997b4908d43c03
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-14 20:44:46 +0000

    Fetched campaign dynamically

--- frontend/src/app/components/campaign-detail/campaign-detail.component.ts
+++ frontend/src/app/components/campaign-detail/campaign-detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Campaign from 'src/app/models/campaign';
+import { CrowdfundingService } from 'src/app/service/crowdfunding.service';
 
 @Component({
   selector: 'app-campaign-detail',
   templateUrl: './campaign-detail.component.html',
   styleUrls: ['./campaign-detail.component.css']
 })
-export class CampaignDetailComponent {
+export class CampaignDetailComponent implements OnInit {
+
+  campaignId!: number;
+  campaign!: Campaign;
+
+  constructor(private route: ActivatedRoute, private crowdfundingService: CrowdfundingService) { }
+
+  ngOnInit(): void {
+    this.campaignId = Number(this.route.snapshot.params['id']);
+    this.crowdfundingService.getCampaign(this.campaignId).then(campaign => {
+      this.campaign = campaign;
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 
 }
