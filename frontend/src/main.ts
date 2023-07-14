d25af78908c93405b313fbfa0f7fd29e561f326d
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-14 20:12:47 +0000

    Added ethereum to polyfill

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-declare global {
-  interface Window { ethereum?: any; }
-}
-window.ethereum = window.ethereum || {};
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
