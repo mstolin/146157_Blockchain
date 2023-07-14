0a2ff580d3926620fb1ab444edcb740a9fc5eeb9
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-14 20:26:09 +0000

    Created campaign detail component

---
+++ frontend/src/app/components/campaign-detail/campaign-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-detail',
+  templateUrl: './campaign-detail.component.html',
+  styleUrls: ['./campaign-detail.component.css']
+})
+export class CampaignDetailComponent {
+
+}
