4748be4643e90eec34c9363b265633e14bdc8698
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-14 20:09:30 +0000

    List all campaigns

--- frontend/src/app/components/all-campaigns/all-campaigns.component.ts
+++ frontend/src/app/components/all-campaigns/all-campaigns.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Campaign from 'src/app/models/campaign';
+import { CrowdfundingService } from 'src/app/service/crowdfunding.service';
 
 @Component({
   selector: 'app-all-campaigns',
   templateUrl: './all-campaigns.component.html',
   styleUrls: ['./all-campaigns.component.css']
 })
-export class AllCampaignsComponent {
+export class AllCampaignsComponent implements OnInit {
 
-  campaigns: { title: string, description: string }[] = [
-    {
-      'title': 'Campaign #1',
-      'description': 'A description for #1'
-    },
-    {
-      'title': 'Campaign #2',
-      'description': 'A description for #2'
-    },
-  ];
+  campaigns: Campaign[] = [];
 
-  constructor() {}
+  constructor(private crowdfundingService: CrowdfundingService) {}
+
+  ngOnInit():void {
+    this.crowdfundingService.getCampaigns().then(campaigns => {
+      this.campaigns = campaigns;
+    }).catch(err => {
+      console.log("ERR", err);
+    });
+  }
 
 }
--- frontend/src/app/components/new-campaign/new-campaign.component.ts
+++ frontend/src/app/components/new-campaign/new-campaign.component.ts
@@ -3,7 +3,10 @@ import { Store } from '@ngrx/store';
 
 import { selectWallet } from '../../state/wallet.selectors';
 import Campaign from 'src/app/models/campaign';
+import BoxOffer from 'src/app/models/boxOffer';
+import Box from 'src/app/models/box';
 import { CrowdfundingService } from 'src/app/service/crowdfunding.service';
+import Web3 from 'web3';
 
 @Component({
   selector: 'app-new-campaign',
@@ -28,20 +31,30 @@ export class NewCampaignComponent {
 
   onSubmit() {
     if (this.title && this.description && this.owner) {
-      const campaign = new Campaign(this.title, this.description, this.owner);
-      console.log(campaign);
+      // 6 weeks from now
+      const campaign = {
+        title: this.title,
+        description: this.description,
+        owner: this.owner,
+        duration: 3600
+      };
+      const boxes = [
+        {
+          total: 2,
+          available: 2,
+          box: {
+            title: 'Very nice box',
+            description: 'This is a great box',
+            price: 2
+          }
+        }
+      ];
 
-      /*this.crowdfundingService.createCampaign(campaign).then(() => {
-        console.log("OK OKOK");
+      this.crowdfundingService.createCampaign(campaign, boxes).then(() => {
+        console.log("OKIDOKI");
       }).catch(error => {
         console.log("ERR:" + error);
-      });*/
-
-      /*this.crowdfundingService.getCampaigns().then(() => {
-        console.log("OK OKOK");
-      }).catch(error => {
-        console.log("ERR:" + error);
-      });*/
+      });
     } else {
       console.log("SOMETHING MISSING");
     }
