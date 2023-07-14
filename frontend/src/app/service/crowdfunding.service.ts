e51d0f621ff02658d5f3417e8f319084b0ac65ca
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-14 20:08:05 +0000

    Added more contract methods to the crowdfunding service

--- frontend/src/app/service/crowdfunding.service.ts
+++ frontend/src/app/service/crowdfunding.service.ts
@@ -1,21 +1,38 @@
 // @ts-nocheck
 
 import { Injectable } from '@angular/core';
-import { ContractAbi } from 'web3';
+import Web3, { ContractAbi } from 'web3';
 
 import Campaign from '../models/campaign';
 import Crowdfunding from '../../assets/abi/Crowdfunding.json';
 import BoxOffer from '../models/boxOffer';
-import Box from '../models/box';
 import ContractService from './contract.service';
+import CampaignRef from '../models/campaignRef';
 
-type CampaignRes = {
+type CampaignResp = {
   title: string;
   description: string;
   owner: string;
-  progress: number;
-  deadline: bigint;
-  collectedAmount: bigint;
+  deadline: string;
+  collectedAmount: string;
+  boxesLeft: string;
+  isStopped: string;
+};
+type CreateCampaignReq = {
+  owner: string,
+  title: string;
+  description: string;
+  duration: number;
+}
+type BoxReq = {
+  title: string;
+  description: string;
+  price: number;
+};
+type BoxOfferReq = {
+  total: number;
+  available: number;
+  box: BoxReq
 };
 
 @Injectable({
@@ -23,7 +40,7 @@ type CampaignRes = {
 })
 export class CrowdfundingService extends ContractService {
 
-  private readonly _contractAddress: string = '0xb9d1f4d6878d22aa50c1da31f40b8c3a88b31b5e';
+  private readonly _contractAddress: string = '0x5Ecc3617a452fa0341C374085b963CAA39c9f699';
 
   constructor() {
     super();
@@ -40,50 +57,135 @@ export class CrowdfundingService extends ContractService {
     }
   }
 
-  async createCampaign(campaign: Campaign): Promise<boolean> {
+  createCampaign(campaign: CreateCampaignReq, boxes: BoxOfferReq[]): Promise<[Campaign, BoxOffer[]]> {
     return new Promise(async (resolve, reject) => {
       const contract = this.getContract();
       if (contract) {
         try {
-          const boxes = [
-            new BoxOffer(new Box('Super box', 'very nice', 100, 2), 1, 1),
-          ];
-
           await contract
-            .methods.createCampaign(campaign.owner, campaign.title, campaign.description, 1000000000, boxes)
+            .methods.createCampaign(campaign.owner, campaign.title, campaign.description, Number(campaign.duration), boxes)
             .send({ 'from': this.selectedAddress });
 
-          resolve(true);
+          resolve([campaign, boxes]);
         } catch(err) {
-          reject(false);
+          reject(err);
         }
       } else {
-        reject(false);
+        reject();
       }
     });
   }
 
-  async getCampaigns(): Promise<Campaign[]> {
+  getCampaigns(): Promise<Campaign[]> {
     return new Promise(async (resolve, reject) => {
       const contract = this.getContract();
       if (contract) {
         try {
-          let campaigns: CampaignRes[] = await contract
+          let campaigns: CampaignResp[] = await contract
             .methods
             .getCampaigns()
             .call();
-          campaigns = campaigns.map(campaign => {
+          campaigns = campaigns.map(res => res.campaign).map(campaign => {
             const deadline = new Date(Number(campaign.deadline) * 1000);
             return new Campaign(
               campaign.title,
               campaign.description,
               campaign.owner,
-              campaign.progress,
               deadline,
-              campaign.collectedAmount);
-            });
-
+              Number(campaign.collectedAmount),
+              Number(campaign.boxesLeft),
+              campaign.isStopped
+            );
+          });
           resolve(campaigns);
+        } catch (err) {
+          reject(err);
+        }
+      } else {
+        reject();
+      }
+    });
+  }
+
+  getCampaign(campaignId: number): Promise<Campaign> {
+    return new Promise(async (resolve, reject) => {
+      const contract = this.getContract();
+      if (contract) {
+        try {
+          let campaign: CampaignResp = await contract
+            .methods
+            .getCampaign(campaignId)
+            .call();
+
+          const deadline = new Date(Number(campaign.deadline) * 1000);
+          campaign = Campaign(
+            campaign.title,
+            campaign.description,
+            campaign.owner,
+            deadline,
+            BigInt(campaign.collectedAmount),
+            BigInt(campaign.boxesLeft),
+            campaign.isStopped
+          );
+
+          resolve(campaign);
+        } catch (err) {
+          reject(err);
+        }
+      } else {
+        reject();
+      }
+    });
+  }
+
+  getBoxes(campaignId: number): Promise<BoxOffer[]> {
+    return new Promise(async (resolve, reject) => {
+      const contract = this.getContract();
+      if (contract) {
+        try {
+          let boxes: BoxOffer[] = await contract
+            .methods
+            .getBoxes(campaignId)
+            .call();
+          resolve(boxes);
+        } catch (err) {
+          reject(err);
+        }
+      } else {
+        reject();
+      }
+    });
+  }
+
+  stopCampaign(campaignId: number): Promise {
+    return new Promise(async (resolve, reject) => {
+      const contract = this.getContract();
+      if (contract) {
+        try {
+          await contract
+            .methods
+            .stopCampaign(campaignId)
+            .send({ 'from': this.selectedAddress });
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      } else {
+        reject();
+      }
+    });
+  }
+
+  buyBox(campaignId: number, boxId: number): Promise {
+    return new Promise(async (resolve, reject) => {
+      const contract = this.getContract();
+      if (contract) {
+        try {
+          await contract
+            .methods
+            .buyBox(campaignId, boxId)
+            .send({ 'from': this.selectedAddress });
+          resolve();
         } catch (err) {
           reject(err);
         }
