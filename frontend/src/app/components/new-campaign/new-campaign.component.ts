dcbf5c93f5fb6c0287177702e89f68c65fa6ffb4
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-16 15:50:46 +0000

    Extended new-campaign form

--- frontend/src/app/components/new-campaign/new-campaign.component.ts
+++ frontend/src/app/components/new-campaign/new-campaign.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { selectWallet } from '../../state/wallet.selectors';
-import Campaign from 'src/app/models/campaign';
-import BoxOffer from 'src/app/models/boxOffer';
-import Box from 'src/app/models/box';
 import { CrowdfundingService } from 'src/app/service/crowdfunding.service';
-import Web3 from 'web3';
 
 @Component({
   selector: 'app-new-campaign',
@@ -20,6 +16,10 @@ export class NewCampaignComponent {
   title?: string;
   description?: string;
   owner?: string;
+  farmer?: string;
+  butcher?: string;
+  deliveryService?: string;
+  deadline?: number;
 
   constructor(private store: Store, private crowdfundingService: CrowdfundingService) {
     this.wallet$.subscribe(wallet => {
