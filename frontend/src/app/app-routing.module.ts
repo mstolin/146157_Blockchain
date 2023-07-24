644d2e7717d98e24002358e8c9774364b42f6d92
Marcel Stolin <kuri-dv@mailbox.org>
2023-07-24 19:48:40 +0000

    Extended routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { AllCampaignsComponent } from './components/all-campaigns/all-campaigns.component';
 import { NewCampaignComponent } from './components/new-campaign/new-campaign.component';
 import { CampaignDetailComponent } from './components/campaign-detail/campaign-detail.component';
+import { BuyBoxComponent } from './components/buy-box/buy-box.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'all-campaigns', component: AllCampaignsComponent },
   { path: 'new-campaign', component: NewCampaignComponent },
-  { path: 'campaign/:id', component: CampaignDetailComponent }
+  { path: 'campaign/:id', component: CampaignDetailComponent },
+  { path: 'campaign/:campaignId/buy/:boxId', component: BuyBoxComponent }
 ];
 
 @NgModule({
