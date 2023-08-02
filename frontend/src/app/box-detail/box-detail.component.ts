1e52ba7b89d67a6f202fb5cb7cfe215bcb4a2699
Marcel Stolin <kuri-dv@mailbox.org>
2023-08-02 18:33:43 +0000

    Created box detail component

---
+++ frontend/src/app/box-detail/box-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-detail',
+  templateUrl: './box-detail.component.html',
+  styleUrls: ['./box-detail.component.css']
+})
+export class BoxDetailComponent {
+
+}
