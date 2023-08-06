8fadfce0ad735d2e6ddb5b863a41a0f324081862
Marcel Stolin <kuri-dv@mailbox.org>
2023-08-06 14:34:52 +0000

    Fixed migrate script

--- smart_contracts/migrations/1_deploy_contracts.js
+++ smart_contracts/migrations/1_deploy_contracts.js
@@ -1,12 +1,5 @@
 const Crowdfunding = artifacts.require("Crowdfunding");
-const CrowdfundingLib = artifacts.require("CrowdfundingLib");
-
-function deployCrowdLib(deployer) {
-  deployer.deploy(CrowdfundingLib);
-  deployer.link(CrowdfundingLib, Crowdfunding);
-}
 
 module.exports = function(deployer) {
-  deployCrowdLib(deployer);
   deployer.deploy(Crowdfunding);
 };
