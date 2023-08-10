b4d10f53ba826fb3d40f9dde38841a9de9277a3d
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2023-08-10 22:25:21 +0200

    Changes in deploy conctracts

--- smart_contracts/migrations/1_deploy_contracts.js
+++ smart_contracts/migrations/1_deploy_contracts.js
@@ -1,5 +1,14 @@
 const Crowdfunding = artifacts.require("Crowdfunding");
+const SupplyChains = artifacts.require("SupplyChains");
 
-module.exports = function(deployer) {
-  deployer.deploy(Crowdfunding);
+module.exports = async function(deployer) {
+  await deployer.deploy(SupplyChains);
+  const SupplyChainsInstance = await SupplyChains.deployed();
+
+  await deployer.deploy(Crowdfunding, SupplyChainsInstance.address);
+  const CrowdfundingInstance = await Crowdfunding.deployed();
+
+  console.log("Crowdfunding address: ", CrowdfundingInstance.address);
+  console.log("SupplyChains address: ", SupplyChainsInstance.address);
+  // deployer.deploy(Crowdfunding);
 };
