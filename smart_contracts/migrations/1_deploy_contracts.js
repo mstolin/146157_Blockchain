72e04eca8e5712b14053040f38eb879005a13528
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2023-08-12 12:13:33 +0200

    Added initialize func in crowdfunding for testing

--- smart_contracts/migrations/1_deploy_contracts.js
+++ smart_contracts/migrations/1_deploy_contracts.js
@@ -1,7 +1,7 @@
 const Crowdfunding = artifacts.require("Crowdfunding");
 const SupplyChains = artifacts.require("SupplyChains");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(SupplyChains);
   const SupplyChainsInstance = await SupplyChains.deployed();
 
@@ -11,4 +11,11 @@ module.exports = async function(deployer) {
   console.log("Crowdfunding address: ", CrowdfundingInstance.address);
   console.log("SupplyChains address: ", SupplyChainsInstance.address);
   // deployer.deploy(Crowdfunding);
+
+  await CrowdfundingInstance.initializeData(accounts[1], accounts[2], accounts[3], accounts[4], "RANDOM_SECRET");
+  console.log("Owner address: ", accounts[1]);
+  console.log("Farmer address: ", accounts[2]);
+  console.log("Butcher address: ", accounts[3]);
+  console.log("Delivery address: ", accounts[4]);
+  console.log("Crowdfunding data initialized");
 };
