67f23a5599eaf75c472665efa813e38dab25d276
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2023-08-14 13:41:36 +0200

    Added wallet to campaign detail

--- frontend/src/app/components/campaign-detail/campaign-detail.component.ts
+++ frontend/src/app/components/campaign-detail/campaign-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+
 import Box from 'src/app/models/box';
 import BoxSellRef from 'src/app/models/boxSellRef';
 import Campaign from 'src/app/models/campaign';
 import { CrowdfundingService } from 'src/app/service/crowdfunding.service';
 import { utils } from 'web3';
-import {SupplyChainService} from "../../service/supplychain.service";
+import { SupplyChainService } from "../../service/supplychain.service";
 import SupplyChain from "../../models/supplychain";
+import {selectWallet} from "../../state/wallet.selectors";
 
 @Component({
   selector: 'app-campaign-detail',
@@ -14,6 +17,9 @@ import SupplyChain from "../../models/supplychain";
   styleUrls: ['./campaign-detail.component.css']
 })
 export class CampaignDetailComponent implements OnInit {
+
+  wallet$ = this.store.select(selectWallet);
+  owner?: string;
 
   // campaign
   campaign!: Campaign;
@@ -24,7 +30,13 @@ export class CampaignDetailComponent implements OnInit {
   // supply chain
   supplychain!: SupplyChain;
 
-  constructor(private route: ActivatedRoute, private crowdfundingService: CrowdfundingService, private supplychainService: SupplyChainService) { }
+  constructor(private store: Store, private route: ActivatedRoute, private crowdfundingService: CrowdfundingService, private supplychainService: SupplyChainService) {
+    this.wallet$.subscribe(wallet => {
+      if (wallet.activeAccount) {
+        this.owner = wallet.activeAccount;
+      }
+    });
+  }
 
   private loadData(campaignId: number) {
     this.crowdfundingService.getCampaign(campaignId).then(campaign => {
@@ -67,4 +79,13 @@ export class CampaignDetailComponent implements OnInit {
     });
   }
 
+  onDelivered() : void {
+    if (this.owner) {
+      this.supplychainService.markAnimalAsDelivered(this.supplychain.campaignRef).then(() => {
+        console.log("Delivered");
+      }).catch((error: any) => {
+        console.log("ERR" + error);
+      });
+    }
+  }
 }
